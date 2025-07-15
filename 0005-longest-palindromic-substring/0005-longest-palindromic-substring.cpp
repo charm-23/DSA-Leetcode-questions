883b9b087de57e96efebd94dbcbab58af4ecb580
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-07-15 17:06:13 +0530

    Time: 111 ms (31.82%), Space: 9.3 MB (92.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        int start=0; 
+        int maxlength=0;
+        string ans= "";
+
+        
+        while(start<s.length()){
+            int end= s.size()-1;
+            while(end>=start){
+                if(s[start]==s[end] && end-start+1>maxlength){
+                    int temp1=start;
+                    int temp2=end;
+                    bool isPalindrome=true;
+
+                    while(temp1<=temp2){
+                        if(s[temp1]!=s[temp2]){
+                            isPalindrome=false;
+                            break;
+                        }
+                        temp1++;
+                        temp2--;
+                    }
+
+                    if(isPalindrome){
+                        maxlength=end-start+1;
+                        ans= s.substr(start,end-start+1);
+                    }
+
+                }    
+                end--;
+            }
+            start++;
+        }
+        return ans;
+        
         
     }
 };
