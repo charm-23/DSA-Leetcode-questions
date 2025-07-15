70315251ce08c67c88acebab58266f19063183a4
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-07-16 00:54:30 +0530

    Time: 0 ms (100%), Space: 11.9 MB (49.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,24 +3,20 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
 
         if(strs.empty()) return "";
-        if(strs.size()==1) return strs[0];
+        string prefix= strs[0];
 
-        int length=INT_MAX;
-        int end=0;
-        for(int i=0; i<strs.size()-1; i++){
-            int j=i+1;
-            int k=0; 
+        for(int i=1; i<strs.size(); i++){
+            int j=0;
 
-            while(k<strs[i].length() && k<strs[j].length() && strs[i][k]==strs[j][k]){
-                k++;
+            while(j<prefix.length() && j<strs[i].length() && prefix[j]==strs[i][j]){
+                j++;
             }
 
-            if(k<length){
-                length=k;
-            }
-            
+            prefix= prefix.substr(0,j);
+
+            if(prefix.length()==0) return "";            
         }
-    if(length==INT_MAX || length==0) return "";
-    return strs[0].substr(0,length);   
+
+        return prefix;     
     }
 };
