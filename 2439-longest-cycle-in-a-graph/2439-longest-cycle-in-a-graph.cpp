e034269bf1e698d8a1a2329fe1472487e48ad2ed
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-08-01 18:15:11 +0530

    Time: 37 ms (48.86%), Space: 110.8 MB (48.72%) - LeetHub

---
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestCycle(vector<int>& edges) {
+        int n=edges.size();
+        vector<int>visited(n,0);
+        vector<int>curr_path(n,0);
+        int maxcycle=-1; 
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(i, edges, curr_path, visited, n,0, maxcycle);
+            }
+        }
+        return maxcycle;
+    }
+    
+    void dfs(int node,vector<int>& edges, vector<int>&curr_path, vector<int>&visited, int n, int cyclen, int &maxcycle){
+        cyclen++;
+        visited[node]=1; 
+        curr_path[node]=cyclen;
+
+        if(edges[node]!=-1){
+            if(!visited[edges[node]]){
+                dfs(edges[node], edges, curr_path, visited, n, cyclen, maxcycle); {
+            
+                }
+            }
+            else if(curr_path[edges[node]]!=0){
+                int currentlength= curr_path[node]-curr_path[edges[node]]+1;
+                maxcycle= max(maxcycle, currentlength);
+            }
+        }
+        curr_path[node]=0;
+         
+    }
+};
