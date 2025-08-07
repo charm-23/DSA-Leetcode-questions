fddf2fa4631ab4d63f78df0bb3d1db587e37d3d6
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-08-08 02:49:09 +0530

    Time: 7 ms (32.85%), Space: 18.6 MB (48.25%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(numCourses,0);
+
+        unordered_map<int,vector<int>>adjlist; 
+
+        for(int i=0; i<prerequisites.size(); i++){
+            int a= prerequisites[i][0];
+            int b= prerequisites[i][1];
+
+            adjlist[b].push_back(a); 
+            indegree[a]++;
+        }
+
+        return bfs(adjlist, indegree, numCourses);
+    }
+
+    vector<int> bfs(unordered_map<int,vector<int>>& adjlist, vector<int>& indegree, int numCourses ){
+        queue<int> q; 
+        vector<int> ans; 
+
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node= q.front(); 
+            q.pop(); 
+            ans.push_back(node);
+
+            for(int ngbr: adjlist[node]){
+                indegree[ngbr]--; 
+
+                if(indegree[ngbr]==0) q.push(ngbr);
+            }
+        }
+
+        if(ans.size()== numCourses){
+            return ans;
+        }
+        else{
+            return {};
+        }
+    }
+};
