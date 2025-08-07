f3fc3666e74eb7eaea56098e4046801d3f56579f
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-08-08 02:25:35 +0530

    Time: 8 ms (37.22%), Space: 21.8 MB (6.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,53 +1,3 @@
-// class Solution {
-// public:
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         vector<int> indegree(numCourses, 0);
-        
-//         unordered_map<int, vector<int>>adjlist; 
-        
-//         for(int i=0; i<prerequisites.size(); i++){
-//             int a= prerequisites[i][0]; 
-//             int b= prerequisites[i][1];
-            
-//             adjlist[a].push_back(b);
-//             indegree[b]++;
-//         }
-
-//         return cycle(indegree, prerequisites, adjlist, numCourses);
-//     }
-
-//     bool cycle(vector<int>& indegree, vector<vector<int>>& prerequisites, unordered_map<int, vector<int>>adjlist, int numCourses){
-//         queue<int> q;
-//         vector<int>ans; 
-
-//         for(int i=0; i<numCourses; i++){
-//             if(indegree[i]==0){
-//                 q.push(i);
-//             }
-//         }
-
-//         while(!q.empty()){
-//             int node= q.front();
-//             q.pop(); 
-//             ans.push_back(node);
-
-//             for(int ngbr: adjlist[node]){
-//                 indegree[ngbr]--; 
-
-//                 if(indegree[ngbr]==0) q.push(ngbr);
-//             }
-//         }
-
-//         if(ans.size()!= numCourses){
-//             return false;
-//         }
-//         else{
-//             return true;
-//         }
-//     }
-
-// };
-
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -59,24 +9,16 @@ public:
             int a= prerequisites[i][0]; 
             int b= prerequisites[i][1];
             
-            adjlist[a].push_back(b);
-            indegree[b]++;
+            adjlist[b].push_back(a);
+            indegree[a]++;
         }
 
-        cycle(indegree, prerequisites, adjlist, numCourses);
-
-        for(int i: indegree){
-            if(i!=0){
-                return false;
-            }
-        }
-
-        return true; 
-
+        return cycle(indegree, prerequisites, adjlist, numCourses);
     }
 
-    void cycle(vector<int>& indegree, vector<vector<int>>& prerequisites, unordered_map<int, vector<int>>& adjlist, int numCourses){
+    bool cycle(vector<int>& indegree, vector<vector<int>>& prerequisites, unordered_map<int, vector<int>>adjlist, int numCourses){
         queue<int> q;
+        vector<int>ans; 
 
         for(int i=0; i<numCourses; i++){
             if(indegree[i]==0){
@@ -87,6 +29,7 @@ public:
         while(!q.empty()){
             int node= q.front();
             q.pop(); 
+            ans.push_back(node);
 
             for(int ngbr: adjlist[node]){
                 indegree[ngbr]--; 
@@ -94,6 +37,63 @@ public:
                 if(indegree[ngbr]==0) q.push(ngbr);
             }
         }
+
+        if(ans.size()!= numCourses){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
 };
+
+// class Solution {
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<int> indegree(numCourses, 0);
+        
+//         unordered_map<int, vector<int>>adjlist; 
+        
+//         for(int i=0; i<prerequisites.size(); i++){
+//             int a= prerequisites[i][0]; 
+//             int b= prerequisites[i][1];
+            
+//             adjlist[b].push_back(a); //b->a
+//             indegree[a]++;
+//         }
+
+//         cycle(indegree, prerequisites, adjlist, numCourses);
+
+//         for(int i: indegree){
+//             if(i!=0){
+//                 return false;
+//             }
+//         }
+
+//         return true; 
+
+//     }
+
+//     void cycle(vector<int>& indegree, vector<vector<int>>& prerequisites, unordered_map<int, vector<int>>& adjlist, int numCourses){
+//         queue<int> q;
+
+//         for(int i=0; i<numCourses; i++){
+//             if(indegree[i]==0){
+//                 q.push(i);
+//             }
+//         }
+
+//         while(!q.empty()){
+//             int node= q.front();
+//             q.pop(); 
+
+//             for(int ngbr: adjlist[node]){
+//                 indegree[ngbr]--; 
+
+//                 if(indegree[ngbr]==0) q.push(ngbr);
+//             }
+//         }
+//     }
+
+// };
