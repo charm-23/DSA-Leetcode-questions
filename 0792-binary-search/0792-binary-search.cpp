fb5c0cc60f0406cdb18a679a1736dd9b05ec667d
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-10-04 00:06:26 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low= 0; 
-        int high= nums.size()-1;
+    
+    //iterative 
+
+    // int search(vector<int>& nums, int target) {
+    //     int low= 0; 
+    //     int high= nums.size()-1;
         
-        while(low<=high){
-            int mid= low + (high-low)/2; 
+    //     while(low<=high){
+    //         int mid= low + (high-low)/2; 
         
-        if(nums[mid]==target){
-            return mid; 
-        }
+    //     if(nums[mid]==target){
+    //         return mid; 
+    //     }
 
-        else if(nums[mid]>target){
-            high= mid-1; 
-        }
+    //     else if(nums[mid]>target){
+    //         high= mid-1; 
+    //     }
 
-        else{ 
-            low= mid+1; 
-        }
+    //     else{ 
+    //         low= mid+1; 
+    //     }
 
-        }
-    return -1; 
+    //     }
+    // return -1; 
+    // }
+
+
+    //recursive 
+    int search(vector<int>& nums, int target){
+        return helper(nums, target, 0, nums.size()-1);
+    }
+
+    int helper(vector<int>&nums, int target, int low, int high){
+        if(low>high) return -1; 
+
+        int mid= low + (high-low)/2; 
+
+        if(nums[mid]==target) return mid; 
+        else if (nums[mid]> target) return helper(nums, target, low, mid-1); 
+        else return helper(nums,target, mid+1, high); 
     }
 };
