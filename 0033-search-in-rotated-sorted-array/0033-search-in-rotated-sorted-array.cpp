1614c784a9930330a7ef73f06c9c3d63f6fb70f3
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-10-06 23:40:22 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -19,35 +19,29 @@ public:
 
     }
 
-
     int search(vector<int>& nums, int target) {
+        int low=0; int high= nums.size()-1; 
 
-        //find smallest element 
-        int low= 0; 
-        int high= nums.size()-1;  
-
-        while(low<high){               //low==high: smallest element right. 
+        while(low<=high){
             int mid= low+ (high-low)/2; 
 
-            if(nums[mid]>nums[high]){
-                low= mid+1;  
+            if(nums[mid]==target) return mid; 
+
+            //left sorted 
+
+            if(nums[low]<=nums[mid]){
+                if(binarysearch(nums, target, low, mid-1)!= -1) return binarysearch(nums, target, low, mid);
+                else low= mid+1;  
+            } 
+
+            //right sorted 
+
+            else{
+                if(binarysearch(nums, target, mid+1, high)!= -1) return binarysearch(nums, target, mid+1 , high);
+                else high= mid-1; ;
             }
-            else {
-                high= mid;
-            }
-            
-        }
 
-        int temp= low;
-
-        if(binarysearch(nums,target,0,temp-1)!=-1){
-            return binarysearch(nums,target,0,temp-1); 
         }
-
-        else {
-            // if(binarysearch(nums,target,temp,nums.size()-1)){
-            return binarysearch(nums,target,temp,nums.size()-1); 
-        }
-        
+    return -1; 
     }
 };
