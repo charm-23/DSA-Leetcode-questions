38a72f7bb3a9f93a33667014a37a0201226019c5
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-10-10 23:49:16 +0530

    Time: 0 ms (100%), Space: 12.4 MB (67.98%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -10,7 +10,9 @@ public:
         while (low<high){
             int mid = low + (high-low)/2;
 
-            if(nums[mid]<nums[mid+1]){
+            if (nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+
+            else if(nums[mid]<nums[mid+1]){
                 low=mid+1; 
             }
 
