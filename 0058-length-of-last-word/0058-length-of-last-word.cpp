64e0b0316874017d93d374271fc5bf67085d12a3
charm-23 <161016003+charm-23@users.noreply.github.com>
2025-10-17 04:12:14 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.49%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        int n=s.length(); 
+        int i=n-1; 
+        while(s[i]==' '){
+            i--; 
+        }
+        int count=0; 
+        while(i>=0 && s[i]!=' '){
+            count++; 
+            i--;
+        }
+    return count;     
         
     }
 };
