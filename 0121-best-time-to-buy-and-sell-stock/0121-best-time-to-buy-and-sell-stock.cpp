3dd9232446c0a437763de912905a69494070cdb8
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-03-29 23:01:59 +0530

    Time: 0 ms (100%), Space: 97.4 MB (59.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int prof=0;
-        int mini= prices[0];
+        //track min so far
+        int maxprof=0; 
+        int mini= prices[0]; 
         for(int i=1; i<prices.size(); i++){
-            prof= max(prof,prices[i]- mini);
-            mini= min(mini, prices[i]); 
+            int cost= prices[i]- mini; 
+            mini=min(mini, prices[i]);
+            maxprof= max(maxprof, cost); 
         }
-    return prof;
+    return maxprof; 
+       
     }
 };
