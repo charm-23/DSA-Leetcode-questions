2d6b04a56a5991ca1b25676783fdeb16c875a9fc
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-04-02 17:34:37 +0530

    Time: 13 ms (92.11%), Space: 60.2 MB (84.03%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0) return 0; 
+        if(nums.size()==1) return 1; 
+        sort(nums.begin(), nums.end()); 
+        int ans=1; 
+        int cnt=1; 
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]-nums[i-1]==1){
+                cnt++;
+                ans= max(cnt, ans);
+            }
+            else if(nums[i]-nums[i-1]==0){
+                continue; 
+            }
+            else{
+                cnt=1;
+            }
+        }
+    return ans;    
+    }
+};
