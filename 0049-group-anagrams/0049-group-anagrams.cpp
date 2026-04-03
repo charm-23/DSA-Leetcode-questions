61a156c641133fb3570051f0f9ac5bad33a91172
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-04-03 13:44:16 +0530

    Time: 23 ms (30.51%), Space: 31.6 MB (14.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> mpp; 
+        map<vector<int>, vector<string>> mpp; 
         for(int i=0; i<strs.size(); i++){
-            string s= strs[i]; 
-            sort(s.begin(),s.end());
-            mpp[s].push_back(strs[i]);
+            vector<int>freq(26,0);  //replacing sorting= TC=O(N*klogk)--> TC=O(N*k); 
+            for(char c: strs[i]){
+                freq[c-'a']++; 
+            }
+            mpp[freq].push_back(strs[i]);
         }
 
     vector<vector<string>>ans; 
