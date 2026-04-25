2aa50babbd7b3758dd114f5762bf8ea7de6378b2
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-04-26 01:08:02 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if(nums.size()==1){
-            if(nums[0]==target) return 0; 
-            else return -1; 
-        }
-        
-        int low=0; int high= nums.size()-1; 
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i+1]<nums[i]){
-                int split=i;
-                if(binarysearch(nums,target,low,split)==-1 && binarysearch(nums,target,split+1,high)==-1) return -1;
-                else if(binarysearch(nums,target,low,split)!=-1) return binarysearch(nums,target,low,split); 
-                else return binarysearch(nums,target,split+1,high); 
+        int low=0; int high=nums.size()-1; int n=nums.size(); 
+        while(low<=high){
+            int mid= low+(high-low)/2; 
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<=nums[high]){ // right half sorted
+                if(nums[mid]<target && target<=nums[high]) low=mid+1; 
+                else high=mid-1;
+            }
+            else{ //left half sorted
+                if(nums[low]<=target && target<nums[mid]) high=mid-1; 
+                else low=mid+1; 
             }
         }
-    return binarysearch (nums, target, low, high);
-    }
-
-    int binarysearch(vector<int>&nums,int target, int low, int high){
-        if(low>high) return -1; 
-        while(low<=high){
-            int mid=low+(high-low)/2; 
-            if(nums[mid]==target) return mid; 
-            else if(nums[mid]<target) low=mid+1;
-            else high= mid-1; 
-        }
-    return -1; 
+    return -1;
     }
 };
