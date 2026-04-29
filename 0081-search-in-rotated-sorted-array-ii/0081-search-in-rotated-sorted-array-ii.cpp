2c2172162cda307064868f6de36d23f305dac417
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-04-29 19:21:03 +0530

    Time: 0 ms (100%), Space: 17.9 MB (93.9%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        set<int> st;
-        for (int i = 0; i<nums.size(); i++) st.insert(nums[i]);
-        return st.find(target) != st.end();
+        int low=0; int high= nums.size()-1; int n=nums.size(); 
+        while(low<=high){
+            int mid= low+ (high-low)/2; 
+            if(nums[mid]==target) return true; 
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+                low++; 
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){ //left half sorted
+                if(nums[low]<=target && target<nums[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else{
+                if(nums[mid]<target && target<=nums[high]) low=mid+1;
+                else high=mid-1;
+            }
+        }
+        return false;   
     }
 };
