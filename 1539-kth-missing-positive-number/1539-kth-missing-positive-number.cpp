e3be32faaf31863c1afc2d76576634f8d92a5e6e
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-05-20 20:03:06 +0530

    Time: 0 ms (100%), Space: 13.4 MB (8.57%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-      //binary search 
-      int l=0; int r=arr.size()-1; 
-      while(l<=r){
-        int mid= l+ (r-l)/2; 
-        if(arr[mid]-(mid+1)<k){
-            l=mid+1;
+        int l=0; int r=arr.size()-1; 
+        while(l<=r){
+            int mid= l+(r-l)/2; 
+            if(arr[mid]-(mid+1)<k){
+                l=mid+1; 
+            }
+            else{
+                r=mid-1; 
+            }
         }
-        else if(arr[mid]-(mid+1)>=k){
-            r=mid-1; 
-        }
-      }
-      return l+k;
+    return l+k;
     }
 };
