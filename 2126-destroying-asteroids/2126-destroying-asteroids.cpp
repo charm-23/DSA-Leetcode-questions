6bd7a77798e5fce2afe56e3d7d9684acd1d2529d
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-05-31 23:30:15 +0530

    Time: 31 ms (66.42%), Space: 106.7 MB (81.61%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end()); 
+
+        long long sum=mass; 
+
+        for(int i: asteroids){
+            if(sum>=i){
+                sum+=i; 
+            }
+            else{
+                return false; 
+            }
+        }
+
+    return true;
+    }
+};
