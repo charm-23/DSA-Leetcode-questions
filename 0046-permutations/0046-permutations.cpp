daa4f235c8570735d39e95c712f42aa7e715f730
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-08 19:17:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.04%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        set<int>s;
-        vector<int>op;
-        solve(ans, nums, s, op); 
+        solve(ans, nums, 0); 
         return ans; 
     }
 
-    void solve(vector<vector<int>>&ans, vector<int>&nums, set<int>&s, vector<int>&op){
-        if(op.size()==nums.size()){
-            ans.push_back(op);
+    void solve(vector<vector<int>>&ans, vector<int>&nums, int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
             return; 
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(s.find(nums[i])==s.end()){
-                op.push_back(nums[i]);
-                s.insert(nums[i]); 
-                solve(ans, nums, s, op); 
-                s.erase(nums[i]);
-                op.pop_back(); 
-            }
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[i], nums[index]); 
+            solve(ans, nums, index+1);
+            swap(nums[i], nums[index]);  
         }
     }
 
