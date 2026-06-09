bb5f72ebbe6e436b8f2d1771bc11b7da8fa4c954
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-09 15:19:05 +0530

    Time: 59 ms (5.97%), Space: 29.2 MB (5.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,55 +2,32 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans; 
+        unordered_set<int>cols; unordered_set<int>diagonal; unordered_set<int>antidiagonal; 
         vector<string>board(n, string(n,'.')); 
-        solve(ans, board, 0, n); 
+        solve(ans, board, 0, n, cols, diagonal, antidiagonal); 
         return ans;
     }
 
-    bool isSafe(vector<string> &board, int row, int col, int n){
-        int temp_row=row; 
-        int temp_col= col; 
-
-        //straight up
-        while(row>0){
-            if(board[row-1][col]=='Q') return false; 
-            row--; 
-        }
-        
-        row= temp_row; 
-        col= temp_col; 
-
-        //diagonal left
-        while(row>0 && col>0){
-            if(board[row-1][col-1]=='Q') return false; 
-            row--; 
-            col--; 
-        }
-
-        row= temp_row; 
-        col= temp_col; 
-
-        // diagonal right
-        while(row>0 && col<n-1){
-            if(board[row-1][col+1]=='Q') return false; 
-            row--; 
-            col++; 
-        }
-
-        return true; 
-    }
-
-    void solve(vector<vector<string>>&ans, vector<string> &board, int row, int n){
+    void solve(vector<vector<string>>&ans, vector<string> &board, int row, int n, unordered_set<int>cols, unordered_set<int>diagonal, unordered_set<int>antidiagonal){
         if(row==n){
             ans.push_back(board); 
             return; 
         }
 
         for(int col=0; col<n; col++){
-            if(isSafe(board, row, col, n)){
+            if(cols.find(col)==cols.end() && diagonal.find(row+col)==diagonal.end() && antidiagonal.find(row-col)==antidiagonal.end()){
                 board[row][col]='Q'; 
-                solve(ans, board,row+1, n); 
+                cols.insert(col); 
+                diagonal.insert(row+col); 
+                antidiagonal.insert(row-col); 
+
+                solve(ans, board, row+1, n, cols, diagonal, antidiagonal); 
+
                 board[row][col]='.'; 
+                cols.erase(col); 
+                diagonal.erase(row+col); 
+                antidiagonal.erase(row-col); 
+
             }
         }
     }
