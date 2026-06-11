7d072a3d79f5459b4d583087772c4c033fe713c5
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-11 20:41:01 +0530

    Time: 4 ms (6.99%), Space: 23.1 MB (19.68%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,23 +10,24 @@
  * };
  */
 
+// optimal TC= O(N)
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
         if(root==NULL) return true;
-        if(abs((maxDepth(root->left))-(maxDepth(root->right)))>1) return false; 
-
-        if(!isBalanced(root->left) || !isBalanced(root->right)) return false; 
-
+        if(maxDepth(root)==-1) return false; 
     return true; 
     }
 
     int maxDepth(TreeNode* node){
         if(node==NULL) return 0; 
+
+        if(maxDepth(node->left)==-1 || maxDepth(node->right)==-1) return -1; 
         
         int lh=1+ maxDepth(node->left); 
         int rh= 1+ maxDepth(node->right);
 
+        if(abs(lh-rh)>1) return -1; 
         return max(lh,rh); 
     }
 };
