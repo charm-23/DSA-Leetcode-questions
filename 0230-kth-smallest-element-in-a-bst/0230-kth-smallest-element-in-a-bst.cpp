038bef92e071aba0fa1c5e8602918f4a2f016282
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-12 21:00:02 +0530

    Time: 0 ms (100%), Space: 24.6 MB (26.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,20 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        stack<TreeNode*>st; 
-        int count=0; 
-        while(true){
-            while(root){
-                st.push(root);
-                root=root->left; 
-            }  
-            root= st.top(); st.pop();
-            count++;
-            if(count==k) return root->val;
-            root=root->right; 
+        queue<int>q;
+        helper(root, k, q); 
+        int ele=1; 
+        while(!q.empty()){
+            if(ele==k) return q.front(); 
+            ele++; 
+            q.pop();
         }
+    return -1;
+    }
+
+    void helper(TreeNode* root, int k, queue<int>&q){
+        if(root==NULL) return;  
+        helper(root->left, k, q);
+        q.push(root->val);
+        helper(root->right,k, q); 
     }
 };
