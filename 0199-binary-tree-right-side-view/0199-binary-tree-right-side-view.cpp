74ea8db24cded88abc7c1901517360cab6977f6b
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-12 17:28:37 +0530

    Time: 0 ms (100%), Space: 15 MB (41.36%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,21 +12,28 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if(root==NULL) return {}; 
         vector<int>ans;
         queue<TreeNode*> q; 
-        q.push(root); 
-        while(!q.empty()){
-            int size= q.size(); 
-            for(int i=0; i<size; i++){
-                if(i==0) ans.push_back(q.front()->val); 
-                TreeNode* node= q.front(); 
-                q.pop(); 
-                if(node->right) q.push(node->right); 
-                if(node->left) q.push(node->left);
-            }
-        }  
-    return ans;  
-    }
 
+        if(!root) return ans;   
+        q.push(root);
+
+        while(!q.empty()){
+            int size= q.size();
+            vector<int>level;
+
+            for(int i=0;i<size;i++){
+                TreeNode* node= q.front();
+                q.pop();
+
+                if(node->left!=NULL) {q.push(node->left);}
+                if(node->right!=NULL) {q.push(node->right);}
+
+                if(i==size-1){
+                    ans.push_back(node->val);
+                }
+            }
+        }
+    return ans;
+    }
 };
