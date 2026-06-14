168c235076efd1835a946f66601ccb30d14df774
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-15 00:48:05 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false; 
-        int sfreq[26]={0}; 
-
-        for(int i=0; i<s.size(); i++){
-            sfreq[s[i]-'a']++;
+        vector<int>freq(26,0); 
+        for(char i:s){
+            freq[i-'a']++;
+        }
+        for(char i: t){
+            freq[i-'a']--;
         }
 
-        int tfreq[26]={0};
-
-        for(int i=0; i<t.size(); i++){
-            tfreq[t[i]-'a']++;
+        for(int i: freq){
+            if(i!=0) return false; 
         }
-
-        for(int i=0; i<26; i++){
-            if(sfreq[i]!=tfreq[i]) return false;
-        }
-
     return true; 
-        
     }
 };
