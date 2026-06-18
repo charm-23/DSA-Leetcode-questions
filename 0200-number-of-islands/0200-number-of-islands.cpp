46340430e27b22619358bb522053accd75dc150b
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-18 16:51:48 +0530

    Time: 23 ms (83.58%), Space: 17.3 MB (50.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//BFS CODE 
+//DFS CODE 
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size(); 
         int m=grid[0].size(); 
@@ -9,7 +9,7 @@ public:
         for(int row=0; row<n; row++){
             for(int col=0; col<m; col++){
                 if(!visited[row][col] && grid[row][col]=='1'){
-                    bfs(row,col,grid, visited, n, m);
+                    dfs(row,col,grid, visited, n, m);
                     cnt++; 
                 }
             }
@@ -17,29 +17,20 @@ public:
     return cnt; 
     }
 
-    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>&visited, int n, int m){
+    void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>&visited, int n, int m){
         visited[row][col]=1;
-        queue<pair<int,int>>q;
-        q.push({row,col});  
 
-        while(!q.empty()){
-            int row= q.front().first; 
-            int col= q.front().second;
-            q.pop();
+        int dx[]= {-1,1,0,0};
+        int dy[]= {0,0,1,-1};
 
-            int dx[]={-1,1,0,0}; 
-            int dy[]={0,0,1,-1};
+        for(int i=0; i<4; i++){
+            int nr= row+ dx[i]; int nc= col+ dy[i];
 
-            for(int i=0; i<4; i++){
-                int nrow= row+ dx[i]; 
-                int ncol= col+ dy[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol]=='1'){
-                    q.push({nrow, ncol}); 
-                    visited[nrow][ncol]=1; 
-                }
+            if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && grid[nr][nc]=='1'){
+                visited[nr][nc]=1; 
+                dfs(nr, nc, grid, visited, n, m);
             }
-        } 
+        }
     }
 
 };
