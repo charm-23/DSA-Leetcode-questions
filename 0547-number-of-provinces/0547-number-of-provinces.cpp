a2a1cecec7c1f0bb3388be01372036fc57d5bc00
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-18 17:32:45 +0530

    Time: 0 ms (100%), Space: 19.6 MB (61.95%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
+        int n= isConnected.size(); 
         vector<int>visited(n,0);
-        unordered_map<int, vector<int>> adjlist; 
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j]==1 && i!=j) adjlist[i].push_back(j);
-            }
-        }
-
-        int provinces=0;
+        int province=0; 
         for(int i=0; i<n; i++){
             if(!visited[i]){
-                provinces++;
-                bfs(visited, adjlist, i);
+                province++; 
+                dfs(i,isConnected,visited,n);  
             }
         }
-        return provinces;
+    return province; 
     }
 
-    void bfs(vector<int>&visited, unordered_map <int, vector<int>> &adjlist, int node){
-        queue<int>q; 
+    void dfs(int node, vector<vector<int>>& isConnected, vector<int>&visited, int n){
         visited[node]=1; 
-        q.push(node); 
-
-        while(!q.empty()){
-            int curr= q.front(); 
-            q.pop(); 
-
-            for(int ngbr: adjlist[curr]){
-                if(!visited[ngbr]){
-                    visited[ngbr]=1;
-                    
-                    q.push(ngbr);
-                }
+        for(int k=0; k<n; k++){
+            if(isConnected[node][k]==1 && !visited[k]){
+                dfs(k, isConnected, visited, n); 
             }
         }
     }
