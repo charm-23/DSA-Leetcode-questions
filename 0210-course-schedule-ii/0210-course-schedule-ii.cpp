0d7c5232b5471db1e262bd93815ebd3bad737054
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-20 20:58:37 +0530

    Time: 0 ms (100%), Space: 18.8 MB (36.79%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> visited(numCourses,0);
         vector<int>ans; 
-        vector<int> curr_path(numCourses,0);
+        vector<int>indegree(numCourses,0);
+        unordered_map<int, vector<int>>adjlist; 
 
-        unordered_map<int,vector<int>>adjlist; 
-
-        for(int i=0; i<prerequisites.size(); i++){
-            int a= prerequisites[i][0];
-            int b= prerequisites[i][1];
+        for(auto &i: prerequisites){
+            int a=i[0]; 
+            int b=i[1];
 
             adjlist[b].push_back(a); 
-        
+            indegree[a]++; 
         }
 
+        queue<int>q; 
         for(int i=0; i<numCourses; i++){
-            if(!visited[i]){
-                if(!dfs(i, visited, adjlist, ans, curr_path)) return {};
-            }
+            if(indegree[i]==0) q.push(i); 
         }
 
-        reverse(ans.begin(), ans.end());
-        return ans; 
-    }
+        while(!q.empty()){
+            int course=q.front(); 
+            q.pop(); 
+            ans.push_back(course); 
 
-    bool dfs(int node, vector<int>& visited, unordered_map<int,vector<int>> &adjlist, vector<int>& ans, vector<int> & curr_path){
-        visited[node]=1; 
-        curr_path[node]=1;
-
-        for(int ngbr: adjlist[node]){
-            if(!visited[ngbr]){
-                if(!dfs(ngbr, visited, adjlist, ans, curr_path)) return false;
+            for(int ngbr: adjlist[course]){
+                indegree[ngbr]--; 
+                if(indegree[ngbr]==0){
+                    q.push(ngbr);
+                }
             }
-
-            else if(visited[ngbr] && curr_path[ngbr]==1) return false;
         }
-        
-        curr_path[node]=0;
-        ans.push_back(node);
-        return true; 
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]!=0) return {};
+        }
+    return ans;
     }
 };
-
-
-// dfs+ backtracking (curr_path) for checking cycles.
-
-// bfs + ans.size()==numCourses/ indegree[nodes]==0 for cycles 
