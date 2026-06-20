1608229154dfb4e868a31271b115af6e7e164003
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-20 12:40:16 +0530

    Time: 3 ms (23.05%), Space: 16.4 MB (10.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,63 +1,68 @@
 class Solution {
 public:
-
-void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>>& visited, int dx[], int dy[]){
-    visited[row][col]=1; 
-
-    int rows= board.size(); 
-    int cols= board[0].size(); 
-
-    for(int k=0; k<4; k++){
-        int ni= row+ dx[k]; 
-        int nj= col+ dy[k]; 
-
-        if(ni>=0 && ni<rows && nj>=0 && nj<cols && !visited[ni][nj] && board[ni][nj]=='O'){
-            dfs(ni, nj, board, visited, dx,dy);
-        }
-    }
-
-}
-
     void solve(vector<vector<char>>& board) {
-        int rows= board.size(); 
-        int cols= board[0].size(); 
-        vector<vector<int>>visited(rows,vector<int>(cols,0)); 
-        int dx[]={1,0,-1,0};
-        int dy[]={0,1,0,-1};
+        int n= board.size(); 
+        int m= board[0].size(); 
+        vector<vector<int>>visited(n, vector<int>(m,0));
 
-        //first row
-        for(int i=0; i<cols; i++){
-            if(!visited[0][i] && board[0][i]=='O'){
-                dfs(0, i, board, visited,  dx, dy);
+        //run bfs for all the edges with O
+
+        for(int j=0; j<m; j++){
+            if(board[0][j]=='O'){
+                bfs(0, j,board, visited, n, m); 
             }
         }
 
-        //last row 
-        for(int i=0; i<cols; i++){
-            if(!visited[rows-1][i] && board[rows-1][i]=='O'){
-                dfs(rows-1, i, board, visited, dx, dy);
+        for(int j=0; j<m; j++){
+            if(board[n-1][j]=='O'){
+                bfs(n-1, j,board, visited, n, m); 
             }
         }
 
-        for(int j=0; j<rows; j++){
-            if(!visited[j][0] && board[j][0]=='O'){
-                dfs(j, 0, board, visited, dx,dy);
+        for(int i=0; i<n; i++){
+            if(board[i][0]=='O'){
+                bfs(i, 0, board, visited, n, m); 
             }
         }
 
-        for(int j=0; j<rows; j++){
-            if(!visited[j][cols-1] && board[j][cols-1]=='O'){
-                dfs(j, cols-1, board, visited,  dx, dy);
+        for(int i=0; i<n; i++){
+            if(board[i][m-1]=='O'){
+                bfs(i,m-1,board, visited, n, m); 
             }
         }
 
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(!visited[i][j] && board[i][j]=='O'){
                     board[i][j]='X'; 
                 }
             }
         }
-        
+    }
+
+    void bfs(int i, int j, vector<vector<char>>& board, vector<vector<int>>&visited, int n, int m){
+        visited[i][j]=1; 
+        queue<pair<int, int>>q; 
+        q.push({i,j}); 
+
+        while(!q.empty()){
+            int row=q.front().first; 
+            int col=q.front().second; 
+            q.pop(); 
+
+            int dx[]={-1,1,0,0}; 
+            int dy[]={0,0,-1,1}; 
+
+            for(int k=0; k<4; k++){
+                int nr= row+ dx[k]; 
+                int nc= col+ dy[k]; 
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && board[nr][nc]=='O'){
+                    visited[nr][nc]=1; 
+                    q.push({nr, nc}); 
+                }
+
+            }
+        }
     }
 };
