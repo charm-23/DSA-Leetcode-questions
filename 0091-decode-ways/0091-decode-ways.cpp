076634399a7246ff57024288affa69af5b1b6f3c
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-23 17:11:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (48.4%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,23 +3,35 @@ public:
     int numDecodings(string s) {
         int n=s.length();
         vector<int>dp(n+1,-1); 
-        helper(0,s,n,dp);  
+        dp[n]=1;
+
+        for(int i=n-1; i>=0; i--){
+            if(s[i]=='0'){
+                dp[i]=0; continue;
+            }
+            int one= dp[i+1]; 
+            int two=0;
+            if((i+1<n) && (s[i]=='1' || s[i]=='2' && '0'<=s[i+1] && s[i+1]<='6')){
+                two= dp[i+2]; 
+            }
+            dp[i]= one+ two; 
+        }  
         return dp[0]; 
     }
 
-    int helper(int i, string &s, int n, vector<int>&dp){
-        if(i==n) return 1;
+    // int helper(int i, string &s, int n, vector<int>&dp){
+    //     if(i==n) return 1;
 
-        if(dp[i]!=-1) return dp[i]; 
+    //     if(dp[i]!=-1) return dp[i]; 
         
-        if(s[i]=='0') return dp[i]=0;
+    //     if(s[i]=='0') return dp[i]=0;
 
-        int one= helper(i+1,s,n,dp);
-        int two=0; 
+    //     int one= helper(i+1,s,n,dp);
+    //     int two=0; 
 
-        if(i+1<n && s[i]=='1' || s[i]=='2' && '0'<=s[i+1] && s[i+1]<='6'){
-            two= helper(i+2,s,n,dp); 
-        }
-        return dp[i]= one+two;  
-    }
+    //     if(i+1<n && s[i]=='1' || s[i]=='2' && '0'<=s[i+1] && s[i+1]<='6'){
+    //         two= helper(i+2,s,n,dp); 
+    //     }
+    //     return dp[i]= one+two;  
+    // }
 };
