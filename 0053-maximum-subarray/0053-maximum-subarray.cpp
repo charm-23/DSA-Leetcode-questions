e32121895dccdbc552567b4191113426e4dec0e3
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-27 02:06:44 +0530

    Time: 0 ms (100%), Space: 71.6 MB (98.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxsum= INT_MIN; 
         int sum=0; 
-
+        int maxsum=INT_MIN; 
         for(int i=0; i<nums.size(); i++){
-            sum+= nums[i]; 
-            maxsum= max(sum, maxsum);
-    
             if(sum<0) sum=0; 
+            sum+=nums[i]; 
+            maxsum=max(sum,maxsum); 
         }
-        return maxsum; 
+    return maxsum; 
     }
 };
