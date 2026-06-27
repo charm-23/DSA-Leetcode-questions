c19fd7da8c4309c650569b3fc898de95f5cd94ba
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-06-27 22:42:33 +0530

    Time: 0 ms (100%), Space: 52.4 MB (58.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size(); 
-        vector<int>dp(n,-1); 
-        dp[n-1]=1; 
-        return helper(nums,0,dp); 
-    }
-
-    bool helper(vector<int>&nums, int index, vector<int>&dp){
-        if(index==nums.size()-1) return dp[nums.size()-1];
-        if(dp[index]!=-1) return dp[index];
-
-        for(int i=1; i<=nums[index]; i++){
-            if(index+i<nums.size()){
-                if(helper(nums, index+i,dp)) return dp[index]=1; 
-            }
+        int n=nums.size();
+        int maxfar=0;
+        for(int i=0; i<n; i++){
+            if(i>maxfar) return false; 
+            maxfar= max(maxfar, i+nums[i]); 
+            if(maxfar>=n-1) return true; 
         }
-    return dp[index]=0;
-    }
+        return true; 
+    }   
 };
