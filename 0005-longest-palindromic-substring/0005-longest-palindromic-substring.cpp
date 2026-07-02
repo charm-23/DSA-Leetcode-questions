e9970eb0c3ad43aeccf1d45dc841df06669ed772
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-02 21:07:21 +0530

    Time: 366 ms (12.71%), Space: 291.7 MB (9.39%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.length(); 
-        int start=0; 
         vector<vector<int>>dp(n, vector<int>(n,-1)); 
-        int maxlen=0; 
-        for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                if(isPalindrome(s,i,j,dp)==true){
-                    int len=j-i+1;
-                    if(len>maxlen) start=i; 
-                    maxlen= max(maxlen, len);
+        int maxlen=1;
+        int start=0;  
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(isPalindrome(i,j,s,dp)){
+                    if(j-i+1>maxlen){
+                        start=i; 
+                        maxlen= j-i+1;
+                    }
                 }
             }
         }
-        return s.substr(start, maxlen); 
+        return s.substr(start,maxlen);  
     }
 
-    bool isPalindrome(string &s, int i, int j, vector<vector<int>>&dp){
+    bool isPalindrome(int i, int j, string &s, vector<vector<int>>&dp){
         if(i>j) return true; 
-        if(dp[i][j]!=-1) return dp[i][j];    
-        if(s[i]!=s[j]) return dp[i][j]=false;
-        return dp[i][j]= isPalindrome(s,i+1, j-1, dp);  
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        if(s[i]!=s[j]) return dp[i][j]=false; 
+        return dp[i][j]= isPalindrome(i+1, j-1, s,dp); 
     }
-};   
+};
