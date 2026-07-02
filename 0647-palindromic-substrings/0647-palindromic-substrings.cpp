8bea0a77e6fdd9fa6338462c84c634a93813ae0f
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-02 21:59:40 +0530

    Time: 3 ms (85.83%), Space: 8.4 MB (99.3%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.size(); 
-        vector<vector<int>>dp(n, vector<int>(n,-1)); 
-        int cnt=0; 
+        int n=s.length(); 
+        int count=0; 
+        //odd length palindromes
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(isPalindrome(i, j, s, dp)){
-                    cnt++; 
-                }
+            int l=i, r=i; 
+            while(l>=0 && r<n && s[l]==s[r]){
+                count++; 
+                l--; r++; 
             }
         }
-        return cnt; 
-    }
 
-    bool isPalindrome(int i, int j, string &s, vector<vector<int>>&dp){
-        if(i>j) return true; 
-        if(dp[i][j]!=-1) return dp[i][j]; 
-        if(s[i]!=s[j]) return dp[i][j]=false; 
-        return dp[i][j]= isPalindrome(i+1, j-1,s, dp); 
+        //even length palindromes
+        for(int i=0; i<n-1; i++){
+            int l=i,r=i+1; 
+            while(l>=0 && r<n && s[l]==s[r]){
+                count++; 
+                l--; r++; 
+            }
+        }
+    return count; 
     }
 };
