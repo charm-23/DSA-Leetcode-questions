3da9ab1621044e0422ea055bbc87ed38762903fa
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-02 16:32:33 +0530

    Time: 4 ms (4.73%), Space: 17.5 MB (88.49%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-//dp stores the minimum cost to reach that index; 
     int minCostClimbingStairs(vector<int>& cost) {
-       int n=cost.size(); 
-       vector<int>dp(n+1,-1); 
-       dp[0]=0; dp[1]=0; 
+        int n=cost.size(); 
+        vector<int>dp(n+1,-1); 
+        dp[0]=0; dp[1]=0; 
 
-       for(int i=2; i<=n; i++){
-        dp[i]= min(dp[i-1]+ cost[i-1], dp[i-2]+ cost[i-2]); 
-       }
-
-       return dp[n];  
+        for(int i=2; i<=n; i++){
+            dp[i]= min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]); 
+        }
+        return dp[n];
     }
 };
