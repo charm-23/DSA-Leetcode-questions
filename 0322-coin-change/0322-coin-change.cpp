11b156df8a4722c1816a10e37cba04d5dac9632a
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-03 15:05:10 +0530

    Time: 47 ms (33.97%), Space: 52.9 MB (14.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,27 +3,30 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size(); 
         vector<vector<int>>dp(n+1, vector<int>(amount+1,-1)); 
-        int res= helper(coins,n, amount,dp); 
-        return res==INT_MAX? -1 : res; 
-    }
+        for(int i=0; i<=n; i++){
+            dp[i][0]=0; 
+        }
 
-    int helper(vector<int>& coins, int i, int amount, vector<vector<int>>&dp){
-        if(amount==0) return 0; 
-        if(i==0) return INT_MAX;
+        for(int i=0; i<=amount; i++){
+            dp[0][i]=INT_MAX; 
+        }
 
-        if(dp[i][amount]!=-1) return dp[i][amount];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                int take=INT_MAX; 
+                if(coins[i-1]<=j){
+                    int res= dp[i][j-coins[i-1]]; 
+                    if(res!=INT_MAX){
+                        take=1+res; 
+                    }
+                }
+                int nottake= dp[i-1][j]; 
 
-        int take=INT_MAX; 
-
-        if(coins[i-1]<=amount){
-            int res=helper(coins, i, amount-coins[i-1],dp); 
-            if(res!=INT_MAX){
-                take= 1+ res;
+                dp[i][j]= min(take,nottake); 
             }
         }
 
-        int nottake= helper(coins, i-1, amount,dp); 
-
-        return dp[i][amount]= min(take,nottake); 
+        if(dp[n][amount]==INT_MAX) return -1; 
+        else return dp[n][amount]; 
     }
 };
