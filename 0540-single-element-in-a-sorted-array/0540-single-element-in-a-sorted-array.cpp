d8d688b1f52caa265cafca22d8a34dafb9420a76
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-04 16:34:07 +0530

    Time: 22 ms (3.08%), Space: 26.2 MB (68.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums){
-        int n= nums.size(); 
-    
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size(); 
         if(n==1) return nums[0]; 
-        if(nums[0]!= nums[1]) return nums[0]; 
-        if(nums[n-1]!= nums[n-2]) return nums[n-1];
+        if(nums[0]!=nums[1]) return nums[0]; 
+        if(nums[n-1]!=nums[n-2]) return nums[n-1]; 
 
-        int low=1; int high= n-2; 
+        int low=2; int high=n-3; 
+
         while(low<=high){
-            int mid= low+(high-low)/2; 
-            if(nums[mid]!= nums[mid-1] && nums[mid]!= nums[mid+1]){
-                return nums[mid]; 
+            int mid= low+ (high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+
+            //each pair should ideally start with even index 
+            if(nums[mid]==nums[mid-1]){
+                if(mid%2==0){
+                    high=mid-1; 
+                }
+                else low=mid+1; 
             }
 
-            if((mid%2==1 && nums[mid-1]==nums[mid]) || (mid%2==0 && nums[mid+1]==nums[mid])) low=mid+1; 
-            else high=mid-1; 
+            else if(nums[mid]==nums[mid+1]){
+                if(mid%2==0) low=mid+1; 
+                else high=mid-1; 
+            }
 
         }
-    return -1; 
+        return nums[low]; 
     }
-    
 };
