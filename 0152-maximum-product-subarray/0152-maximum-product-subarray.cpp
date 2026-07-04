096fc23ff259a4bf3ce8ce724cabcf3f96dc23f8
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-04 17:14:54 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.12%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxproduct=1, minproduct=1;
-        vector<int>dp(nums.size()+1);
-        dp[0]=1; 
-        int ans= *max_element(nums.begin(), nums.end()); 
+        int maxproduct=1, minproduct=1; 
+        int ans=*max_element(nums.begin(), nums.end()); 
 
-        for(int i=1; i<=nums.size(); i++){
-            if(nums[i-1]==0){
-                minproduct=1; 
-                dp[i]=1; 
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                maxproduct=1, minproduct=1; continue;  
             }
-            dp[i]= max(minproduct*nums[i-1],max(nums[i-1], dp[i-1]*nums[i-1])); 
-            minproduct= min(minproduct*nums[i-1],min(nums[i-1], dp[i-1]*nums[i-1])); 
-            ans= max(ans, dp[i]); 
-        }
+            int temp= maxproduct;
+            maxproduct= max(nums[i],max(nums[i]*maxproduct, nums[i]*minproduct));
+            minproduct= min(nums[i],min(nums[i]*temp, nums[i]*minproduct));
 
+            ans= max(maxproduct, ans); 
+        }
     return ans; 
     }
 };
