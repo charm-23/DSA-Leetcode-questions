fab41ee03e98b3c9be703acca447b1883a8fe1ae
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-07 00:49:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (20.22%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return helper(s, p, 0, 0); 
+        int n=s.length(); int m=p.length(); 
+        vector<vector<int>>dp(n+1, vector<int>(m+1,-1)); 
+        return helper(s, p, 0, 0,dp); 
     }
 
-    bool helper(string &s, string &p, int i, int j){
+    bool helper(string &s, string &p, int i, int j, vector<vector<int>>&dp){
         if(i==s.length() && j==p.length()) return true; 
         if(j==p.length()) return false; 
+        
+        if(dp[i][j]!=-1) return dp[i][j]; 
 
         bool notconsider= false;  bool consider=false; 
         if(j+1<p.length() && p[j+1]=='*'){
-            notconsider= helper(s,p,i,j+2); 
+            notconsider= helper(s,p,i,j+2,dp); 
 
             if(i<s.length() && (s[i]==p[j] || p[j]=='.')){
-                consider= helper(s,p,i+1,j); 
+                consider= helper(s,p,i+1,j,dp); 
             }
         }
 
-        else if(i<s.length() && (s[i]==p[j] || p[j]=='.')) return helper(s, p, i+1, j+1); 
+        else if(i<s.length() && (s[i]==p[j] || p[j]=='.')) return dp[i][j]= helper(s, p, i+1, j+1,dp); 
 
-        return consider|| notconsider; 
+        return dp[i][j]=consider|| notconsider; 
     }
 };
