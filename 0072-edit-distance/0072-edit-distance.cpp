883d1de5408d09f8eefe808156d959ee6fd743b9
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-06 20:16:48 +0530

    Time: 3 ms (91.06%), Space: 12.9 MB (81.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size(); 
-        int m= word2.size(); 
-        vector<vector<int>>dp (n+1, vector<int>(m+1));
-        dp[0][0]=0; 
-        for(int i=1; i<=n; i++){
-            dp[i][0]= i; 
-        }
-        for(int j=1; j<=m; j++){
-            dp[0][j]= j; 
-        }
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(word1[i-1]== word2[j-1]){
-                 dp[i][j]= dp[i-1][j-1];
-                }
-                else{
-                    int deleteo= 1+ dp[i-1][j];  
-                    int replace= 1+ dp[i-1][j-1];  
-                    int insert= 1+ dp[i][j-1]; 
-                    dp[i][j]= min(insert,min(deleteo, replace)); 
-                }
-            }
-        }
-
-        return dp[n][m];
+        int n=word1.length(); int m=word2.length(); 
+        vector<vector<int>>dp(n, vector<int>(m,-1)); 
+        return helper(word1, word2, n-1, m-1, dp); 
     }
 
-    // int helper(string word1, string word2, int i, int j, vector<vector<int>>dp){ 
-    //     if(i==0 && j==0) return 0; 
-    //     else if(i==0) return j; // insert
-    //     else if(j==0) return i; // delete 
+    int helper(string &word1, string &word2, int i, int j, vector<vector<int>>&dp){ 
+        if(i<0 && j<0) return 0; 
+        if(i<0) return j+1; 
+        if(j<0) return i+1; 
+        
+        if(dp[i][j]!=-1) return dp[i][j]; 
 
-    //     if(dp[i][j]!= -1) return dp[i][j]; 
-
-    //     else{
-    //         if(word1[i-1]== word2[j-1]){
-    //             return dp[i][j]= helper(word1, word2, i-1, j-1,dp);
-    //         }
-    //         else{
-    //             int deleteo= 1+ helper(word1, word2, i-1, j,dp);  
-    //             int replace= 1+ helper(word1, word2, i-1, j-1,dp);  
-    //             int insert= 1+ helper(word1, word2, i, j-1,dp); 
-
-    //             return dp[i][j]= min(insert,min(deleteo, replace)); 
-    //         }
-    //     }
-    // }
+        if(word1[i]==word2[j]){
+            return dp[i][j]= helper(word1, word2, i-1, j-1,dp); 
+        }
+        else{
+            return dp[i][j]= min(1+ helper(word1, word2, i-1, j-1, dp), min(1+ helper(word1, word2, i, j-1,dp), 1+ helper(word1, word2, i-1,j, dp))); 
+        }
+    }
 };
