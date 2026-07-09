b4a7f86c3ec2d792fe6c56cac1cd035e77624ea6
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-10 00:10:17 +0530

    Time: 210 ms (16.04%), Space: 14 MB (6.88%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
+        //O(n^3)
         int n=nums.size(); 
-        vector<vector<int>>dp(n+2, vector<int>(n+2,-1)); 
         vector<int>balloons(n+2,1); 
+        vector<vector<int>>dp(n+2, vector<int>(n+2,-1));
+
         for(int i=0; i<nums.size(); i++){
-            balloons[i+1]= nums[i]; 
+            balloons[i+1]= nums[i];
         }
 
-        return helper(1, n, balloons, dp); 
+        return helper(balloons, 1, n,dp); 
     }
 
-        int helper(int i, int j, vector<int>&balloons, vector<vector<int>>&dp){
-            if(i>j) return 0; 
+    int helper(vector<int>&balloons, int i, int j, vector<vector<int>>&dp){
+        if(i>j) return 0; 
 
-            if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        int maxcoins=0; 
+        for(int k=i; k<=j; k++){
+            int left= helper(balloons, i, k-1,dp); 
+            int right= helper(balloons, k+1, j,dp); 
 
-            int maxcoins=0; 
-
-            for(int k=i; k<=j; k++){
-                int left= helper(i,k-1,balloons, dp); 
-                int right=helper(k+1,j,balloons, dp); 
-
-                int coins= balloons[i-1]* balloons[k]* balloons[j+1]; 
-                
-                int total= left + coins + right; 
-
-                maxcoins= max(maxcoins, total); 
-            } 
-        return dp[i][j]= maxcoins; 
+            int coins= left+ right+ (balloons[i-1]*balloons[k]*balloons[j+1]); 
+            maxcoins= max(maxcoins, coins); 
         }
+        return dp[i][j]= maxcoins; 
+    }
 };
