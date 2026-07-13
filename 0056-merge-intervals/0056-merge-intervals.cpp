d8d0a5f8f9120584f3089d8beca7e7177f68bce3
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-13 16:47:25 +0530

    Time: 7 ms (38.48%), Space: 24 MB (20.52%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans; 
+        vector<vector<int>>ans;
+        int n=intervals.size(); 
 
+        if(intervals.size()==0 || intervals.size()==1) return {intervals}; 
+        int i=1; 
         sort(intervals.begin(), intervals.end()); 
 
-        vector<int> current= intervals[0]; 
+        int start= intervals[0][0]; 
+        int end= intervals[0][1]; 
 
-        for(int i=1; i<intervals.size(); i++){
-            //overlapping 
-            if(current[1]>= intervals[i][0]){
-                current[1]= max(current[1], intervals[i][1]);
+        while(i<n){
+            while(i<n && intervals[i][0]<=end){
+                end= max(end, intervals[i][1]); 
+                i++;
             }
-             
-            // no overlapping 
-            else{
-                ans.push_back(current);
-                current= intervals[i]; 
+
+            ans.push_back({start,end});
+
+            if(i<n){
+                start= intervals[i][0]; 
+                end= intervals[i][1];
             }
         }
-
-        ans.push_back(current); 
-        
     return ans; 
-        
     }
 };
