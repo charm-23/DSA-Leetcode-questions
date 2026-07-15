061be5089a78c91349bbd4796f0391f918725b72
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-15 16:50:16 +0530

    Time: 0 ms (100%), Space: 17.9 MB (64.31%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<pair<int, int>>>adjlist; 
+        unordered_map<int, vector<pair<int, int>>>adjlist;
 
         for(auto &i: flights){
-            int u= i[0]; 
+            int u= i[0];
             int v= i[1]; 
             int price= i[2]; 
 
             adjlist[u].push_back({price, v}); 
         }
-        vector<int>dist(n,INT_MAX);
 
-        //BFS --> K+1 levels 
+        vector<int>dist(n+1, 1e9); 
 
-        queue<pair<int, int>>q; 
-        q.push({0, src}); 
-        int level=0; 
-        int ans=INT_MAX; 
+        queue<pair<int,int>>q; 
+        q.push({0,src}); 
+        int steps=0; 
+        int ans= 1e9; 
 
-        while(!q.empty() && level<=k+1){
+        while(!q.empty() && steps<=k+1){
             int size= q.size();
 
             for(int i=0; i<size; i++){
                 int node= q.front().second; 
                 int price= q.front().first; 
 
-                if(node==dst){ 
+                if(node==dst){
                     ans= min(ans, price); 
                 }
+
                 q.pop(); 
 
-                for(auto &ngbr: adjlist[node]){
-                    int ngbrprice=ngbr.first; 
-                    int flight= ngbr.second; 
-                    
-                    if (price + ngbrprice < dist[flight]) {
-                        dist[flight] = price + ngbrprice;
-                        q.push({dist[flight], flight}); 
+                for(auto& ngbr: adjlist[node]){
+                    if(price+ ngbr.first < dist[ngbr.second]){
+                        dist[ngbr.second]= price+ ngbr.first; 
+                        q.push({price+ ngbr.first, ngbr.second}); 
                     }
                 }
-            }
-            level++; 
-        }
 
-        return ans==INT_MAX? -1: ans; 
+            }
+            steps++;
+        }
+        return ans==1e9? -1: ans ; 
     }
-};
+}; 
