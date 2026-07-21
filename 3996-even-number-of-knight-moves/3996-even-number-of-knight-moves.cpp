a42c34aebf075da48d437104036bb7b7e9776536
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-21 17:43:36 +0530

    Time: 0 ms (100%), Space: 49.7 MB (70.6%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
@@ -1,34 +1,6 @@
 class Solution {
 public:
     bool canReach(vector<int>& start, vector<int>& target) {
-        int n=8; 
-        vector<vector<bool>>visited(8, vector<bool>(8,false)); 
-
-        int r=start[0]; int c=start[1]; 
-        visited[r][c]=true; 
-        int targetx= target[0]; int targety= target[1]; 
-
-        return helper(r, c, targetx, targety, 0, visited); 
-
-    }
-
-    bool helper(int r, int c, int targetx, int targety, int moves, vector<vector<bool>>&visited){
-        if(moves%2==0 && r==targetx && c==targety) return true; 
-        else if(r==targetx && c==targety) return false; 
-
-        int dx[]={-2,-2,-1,1,2,2,1,-1};
-        int dy[]={-1,1,2,2,1,-1,-2,-2};
-
-        for(int i=0; i<8; i++){
-            int nr= r +dx[i];
-            int nc= c +dy[i]; 
-
-            if(nr<8 && nr>=0 && nc<8 && nc>=0 && visited[nr][nc]==false){
-                visited[nr][nc]=true; 
-                if(helper(nr, nc, targetx, targety, moves+1, visited)) return true;
-            }
-        }
-
-        return false;
+       return ((start[0]+ start[1])%2== (target[0]+target[1])%2);
     }
 };
