1036ae59a4a8bc6bfaefcff334708a70d7c919e1
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-26 16:08:04 +0530

    Time: 0 ms (100%), Space: 8.1 MB (92.7%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     bool isRobotBounded(string instructions) {
-        int x=0; int y=0; char face= 'n'; 
-        for(int i=0; i<instructions.size(); i++){
-            if(instructions[i]=='G'){
-                if(face=='n') y++; 
-                else if(face=='s') y--; 
-                else if(face=='e') x++; 
-                else x--; 
+        int x=0; int y=0;
+        int dir=0; 
+
+        vector<pair<int,int>>directions{{0,1}, {1,0}, {0,-1}, {-1,0}}; 
+
+        for(char c: instructions){
+            if(c=='G'){
+                x+= directions[dir].first; 
+                y+= directions[dir].second; 
             }
-            else if(instructions[i]=='L'){
-                if(face=='n') face='w'; 
-                else if(face=='w') face='s'; 
-                else if(face=='s') face='e'; 
-                else face='n'; 
+            else if(c=='L'){
+                dir= (dir+3)%4; 
             }
-            else{
-                if(face=='n') face='e'; 
-                else if(face=='e') face='s'; 
-                else if(face=='s') face='w'; 
-                else face='n'; 
-            }
+            else dir=(dir+1)%4; 
         }
-        if((x==0 && y==0) || face!='n') return true; 
-        else return false; 
+
+        return ((x==0 && y==0) || dir!=0); 
     }
 };
