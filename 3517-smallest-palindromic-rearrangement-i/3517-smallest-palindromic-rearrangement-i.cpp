816dd4c604d4151646408f9673d8eefa60bb142b
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-07-28 15:34:37 +0530

    Time: 157 ms (15.16%), Space: 62 MB (74.94%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -3,34 +3,34 @@ public:
     string smallestPalindrome(string s) {
         map<char, int>freq; 
         for(char c: s){
-            freq[c]++; 
+            freq[c]++;  // Nlogk k=26 -->N
         }
 
         int n=s.length();
         string ans(n, ' '); 
         int t=0; int j=n-1; 
+        char middle= 0; 
 
-        for(auto &i: freq){
-            int ch= i.first; 
+        for(auto &i: freq){      // O(N)-->N<=26 -->O(1)
+            char ch= i.first; 
             int cnt=i.second;
 
-            for(int k=0; k<cnt/2; k++){
+            for(int k=0; k<cnt/2; k++){       // N/2
                 ans[t]=ch;
                 t++; 
                 ans[j]=ch; 
                 j--; 
             }
-
-            i.second=cnt%2;
+            if(cnt%2==1) middle=ch; 
         }
 
-        for(auto&i: freq){
-            if(i.second==1){
-                ans[t]=i.first; 
-                t++; 
-            }
+        if(middle!=0){
+            ans[t]=middle; 
         }
 
         return ans; 
     }
 };
+
+//tc--> O(N)
+//SC-->O(K=26)+ O(N)--> O(N)
