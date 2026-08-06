4aec9c83acbed5c4026f5f863111c22a265a1abf
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-06 14:53:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        for(int i=n; i<=100; i++){
+            int prod=1; 
+            int temp=i; 
+            while(temp!=0){
+                int digit=temp%10; 
+                prod= prod*digit; 
+                temp=temp/10; 
+            }
+            if(prod%t==0) return i; 
+        }
+        return -1; 
+    }
+};
