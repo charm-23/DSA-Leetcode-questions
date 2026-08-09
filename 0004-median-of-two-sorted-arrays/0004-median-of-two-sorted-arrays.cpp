f97c034e4281af5e753069654ad96aa4fb4a905a
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-09 16:58:33 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.94%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(); int m=nums2.size(); 
-        int i=0; int j=0; 
+       int n1=nums1.size(); int n2=nums2.size(); 
+       if(n1>n2) return findMedianSortedArrays(nums2, nums1); //will calculate using shortest array
 
-        if(n==0 && m==0) return NULL; 
+       int low=0; int high=n1; //telling number of elements not index; 
+       int left= (n1+n2+1)/2; //odd length-->max elements on left, even length -->equal 
 
-        vector<int>merged; 
+       while(low<=high){
+        int mid1= low+(high-low)/2; 
+        int mid2= left-mid1; 
 
-        while(i<n && j<m){
-            if(nums1[i]<nums2[j]){
-                merged.push_back(nums1[i]);
-                i++; 
-            }
-            else if(nums1[i]==nums2[j]){
-                merged.push_back(nums1[i]); 
-                merged.push_back(nums2[j]); 
-                i++; j++; 
-            }
-            else{
-                merged.push_back(nums2[j]); 
-                j++; 
-            }
+        int l1=INT_MIN, l2= INT_MIN; 
+        int r1=INT_MAX, r2= INT_MAX;
+
+        if(mid1-1>=0) l1= nums1[mid1-1]; 
+        if(mid2-1>=0) l2= nums2[mid2-1];
+
+        if(mid1<n1) r1=nums1[mid1]; 
+        if(mid2<n2) r2=nums2[mid2]; 
+
+        //conditions
+
+        if(l1<=r2 && l2<=r1){
+            if((n1+n2)%2==1) return max(l1,l2); 
+            return((double)max(l1,l2)+ min(r1,r2))/2; 
         }
+        else if(l1>r2) high=mid1-1; 
+        else low=mid1+1; 
+       }
 
-        while(i<n){
-            merged.push_back(nums1[i]);
-                i++;
-        }
-
-        while(j<m){
-            merged.push_back(nums2[j]); 
-                j++; 
-        }
-
-    int low=0; int high=n+m-1; 
-    int mid=low+ (high-low)/2; 
-
-    if((n+m)%2==0){
-        return (double)(merged[mid]+ merged[mid+1])/2; 
-    }
-
-    return (double)(merged[mid]); 
+       return -1; 
     }
 };
