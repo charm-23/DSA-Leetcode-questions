10a644797ffa0e1797ba1a12c774b5e4b0bfd28a
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-10 18:02:02 +0530

    Time: 51 ms (42.66%), Space: 51 MB (19.52%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        map<int, int> mpp; // cummulative sum, freq; 
-        int cnt=0; 
-        mpp[0]=1; 
-        int sum=0; 
+        unordered_map<int, int>mpp; 
+        mpp[0]=1;
 
-        for(int i=0; i<n; i++){
-            sum+= nums[i]; 
-            if(mpp.find(sum-k)!= mpp.end()){
-                cnt+=mpp[sum-k]; 
-            }
-            mpp[sum]++; 
+        int sum=0; 
+        int ans=0; 
+
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i]; 
+            ans+= mpp[sum-k]; 
+            mpp[sum]++;
         }
-    return cnt;     
+
+        return ans; 
     }
 };
