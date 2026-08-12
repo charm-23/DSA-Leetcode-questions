acb9f42872d81bf051f55fe2c44c3af6aaa8e042
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-13 00:33:34 +0530

    Time: 67 ms (46.74%), Space: 149.4 MB (27.87%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp; 
+        int n=nums.size(); 
+        int l=0; int r=0; 
+        int ans=0; 
+
+        while(r<n){
+            mpp[nums[r]]++; 
+
+            if(mpp[nums[r]]<=k){
+                ans= max(ans, r-l+1); 
+            }
+            else{
+                while(mpp[nums[r]]>k){
+                    mpp[nums[l]]--; 
+                    l++; 
+                    ans= max(ans, r-l+1); 
+                }
+            }
+            r++; 
+        }
+
+        return ans; 
+    }
+};
