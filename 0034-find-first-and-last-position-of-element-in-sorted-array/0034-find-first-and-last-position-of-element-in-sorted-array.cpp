940515c7377882f4e0ba4fd046ed1e95efb3ca7e
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-13 22:18:18 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size()==0) return {-1,-1};
-        int low=0; int high= nums.size()-1; int n=nums.size(); 
-        int first=-1; int last=-1; 
+        vector<int>ans; 
+        ans.push_back(first(nums, target)); 
+        ans.push_back(last(nums, target)); 
 
-        while(low<=high){
+        return ans; 
+    }
+
+    int first(vector<int>& nums, int target){
+        int first=-1;
+        int low=0; int high= nums.size()-1; 
+
+        while(low<=high){ 
             int mid= low+ (high-low)/2; 
 
             if(nums[mid]==target){
-                first=mid; last=mid; 
-                while(first>0 && nums[first-1]==target){
-                    first--;
-                }
-                while(last<n-1 && nums[last+1]==target){
-                    last++;
-                }
-                return {first,last};
+                first=mid; 
+                high=mid-1; 
             }
-            else if(nums[mid]<target){
-                low=mid+1;
+            else if(nums[mid]>target){
+                high=mid-1; 
             }
             else{
-                high=mid-1;
+                low=mid+1; 
             }
         }
-    return {-1,-1};
+        return first; 
+    }
+
+    int last(vector<int>& nums, int target){
+        int last=-1;
+        int low=0; int high= nums.size()-1; 
+
+        while(low<=high){ 
+            int mid= low+ (high-low)/2; 
+
+            if(nums[mid]==target){
+                last=mid; 
+                low=mid+1; 
+            }
+            else if(nums[mid]>target){
+                high=mid-1; 
+            }
+            else{
+                low=mid+1; 
+            }
+        }
+        return last; 
     }
 };
