e77c0c592a74741420b12d91f81ac19be5576ee7
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-15 14:34:53 +0530

    Time: 19 ms (96.35%), Space: 24.5 MB (13.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
-//DFS CODE 
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size(); 
-        int m=grid[0].size(); 
+        int m= grid[0].size();
+
         vector<vector<int>>visited(n, vector<int>(m,0)); 
-        int cnt=0; 
-        for(int row=0; row<n; row++){
-            for(int col=0; col<m; col++){
-                if(!visited[row][col] && grid[row][col]=='1'){
-                    dfs(row,col,grid, visited, n, m);
-                    cnt++; 
+        int island=0; 
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visited[i][j]==0 && grid[i][j]=='1'){
+                    bfs(i, j, n, m, grid,visited);
+                    island++; 
                 }
             }
         }
-    return cnt; 
+        return island; 
     }
 
-    void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>&visited, int n, int m){
-        visited[row][col]=1;
+    void bfs(int i, int j, int n, int m, vector<vector<char>>& grid, vector<vector<int>>&visited){
+        queue<pair<int,int>>q; 
+        q.push({i,j}); 
+        int dx[]={-1,0,1,0}; 
+        int dy[]={0,1,0,-1}; 
 
-        int dx[]= {-1,1,0,0};
-        int dy[]= {0,0,1,-1};
+        while(!q.empty()){
+            int x= q.front().first; 
+            int y= q.front().second; 
+            q.pop(); 
 
-        for(int i=0; i<4; i++){
-            int nr= row+ dx[i]; int nc= col+ dy[i];
+            for(int k=0; k<4; k++){
+                int nx= x+ dx[k]; 
+                int ny= y+ dy[k]; 
 
-            if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && grid[nr][nc]=='1'){
-                visited[nr][nc]=1; 
-                dfs(nr, nc, grid, visited, n, m);
+                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && grid[nx][ny]=='1'){
+                    visited[nx][ny]=1;
+                    q.push({nx, ny}); 
+                }
             }
         }
     }
-
 };
