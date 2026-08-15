4834cf82a8d85f4adfca14683514fe5c28886cdd
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-15 23:25:22 +0530

    Time: 6 ms (50.02%), Space: 19.9 MB (38.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-//topological sort/ cycle detection in BFS using KHAN'S ALGO 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>>adjlist;
         vector<int>indegree(numCourses,0); 
+        unordered_map<int, vector<int>>neighbours; 
 
-        for(auto &i: prerequisites){
-            int a= i[0];    
-            int b= i[1];
-
-            adjlist[b].push_back(a);
-            indegree[a]++;  
-        }
+        for(auto&i: prerequisites){
+            int a=i[0]; int b=i[1]; 
+            indegree[a]++; 
+            neighbours[b].push_back(a);
+        } 
 
         queue<int>q; 
+
         for(int i=0; i<numCourses; i++){
-            if(indegree[i]==0){
-                q.push(i); 
-            }
+            if(indegree[i]==0) q.push(i); 
         }
 
         while(!q.empty()){
-            int course=q.front(); 
+            int x=q.front(); 
             q.pop(); 
 
-            for(int ngbr: adjlist[course]){
-                indegree[ngbr]--; 
-                if(indegree[ngbr]==0){ 
-                    q.push(ngbr); 
-                }
+            for(int i: neighbours[x]){
+                indegree[i]--; 
+                if(indegree[i]==0) q.push(i); 
             }
         }
 
-        for(int i=0; i<numCourses; i++){
+        for(int i=0; i<indegree.size(); i++){
             if(indegree[i]!=0) return false; 
         }
 
-    return true; 
+        return true; 
     }
 };
