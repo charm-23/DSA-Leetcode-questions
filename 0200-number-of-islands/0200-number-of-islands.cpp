2538511e53c5a2582134ed765d2a33774d249600
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-15 14:44:01 +0530

    Time: 21 ms (91.88%), Space: 17.5 MB (47.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -10,7 +10,7 @@ public:
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(visited[i][j]==0 && grid[i][j]=='1'){
-                    bfs(i, j, n, m, grid,visited);
+                    dfs(i, j, n, m, grid,visited);
                     island++; 
                 }
             }
@@ -18,25 +18,18 @@ public:
         return island; 
     }
 
-    void bfs(int i, int j, int n, int m, vector<vector<char>>& grid, vector<vector<int>>&visited){
-        queue<pair<int,int>>q; 
-        q.push({i,j}); 
-        int dx[]={-1,0,1,0}; 
-        int dy[]={0,1,0,-1}; 
+    void dfs(int i, int j, int n, int m,vector<vector<char>>& grid, vector<vector<int>>&visited){
+        visited[i][j]=1; 
+        int dx[]={-1,1,0,0}; 
+        int dy[]={0,0,-1,1}; 
 
-        while(!q.empty()){
-            int x= q.front().first; 
-            int y= q.front().second; 
-            q.pop(); 
+        for(int k=0; k<4; k++){
+            int nx=i+ dx[k]; 
+            int ny=j+ dy[k]; 
 
-            for(int k=0; k<4; k++){
-                int nx= x+ dx[k]; 
-                int ny= y+ dy[k]; 
+            if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]=='1' && !visited[nx][ny]){
+                dfs(nx, ny,n, m , grid, visited); 
 
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && grid[nx][ny]=='1'){
-                    visited[nx][ny]=1;
-                    q.push({nx, ny}); 
-                }
             }
         }
     }
