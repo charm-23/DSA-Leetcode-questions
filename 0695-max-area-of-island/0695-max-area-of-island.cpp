ae243ba15350e884ae13965fdee19ce46c5bd5b6
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-15 15:01:09 +0530

    Time: 3 ms (52.66%), Space: 28.7 MB (38.45%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size(); 
-        int n= grid[0].size(); 
-        vector<vector<int>>visited(m, vector<int>(n,0)); 
-        int maxarea=0; 
+        int n=grid.size(); 
+        int m= grid[0].size();
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    int area=0; 
-                    bfs(grid, visited, i, j, area, m, n); 
-                    maxarea= max(maxarea,area); 
+        vector<vector<int>>visited(n, vector<int>(m,0)); 
+        int ans=0; 
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visited[i][j]==0 && grid[i][j]==1){
+                    ans= max(ans,dfs(i, j, n, m, grid,visited));
                 }
             }
         }
-        return maxarea; 
+        return ans; 
     }
 
-    void bfs(vector<vector<int>>& grid, vector<vector<int>>&visited, int i, int j, int &area, int m, int n){
+    int dfs(int i, int j, int n, int m,vector<vector<int>>& grid, vector<vector<int>>&visited){
         visited[i][j]=1;
-        queue<pair<int, int>>q; 
-        q.push({i, j});
+        int area=1; 
+        int dx[]={-1,1,0,0}; 
+        int dy[]={0,0,-1,1}; 
 
-        while(!q.empty()){
-            int row=q.front().first; 
-            int col= q.front().second; 
-            area++; 
-            q.pop(); 
+        for(int k=0; k<4; k++){
+            int nx=i+ dx[k]; 
+            int ny=j+ dy[k]; 
 
-            int dx[]={-1,1,0,0};
-            int dy[]={0,0,-1,1}; 
-        
-            for(int k=0; k<4; k++){
-                int nrow= row+ dx[k];
-                int ncol= col+ dy[k]; 
+            if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1 && !visited[nx][ny]){
+                area= area+ dfs(nx, ny,n, m , grid, visited); 
 
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && !visited[nrow][ncol]){
-                    visited[nrow][ncol]=1;
-                    q.push({nrow, ncol}); 
-                }
             }
-        } 
+        }
+        return area; 
     }
 };
