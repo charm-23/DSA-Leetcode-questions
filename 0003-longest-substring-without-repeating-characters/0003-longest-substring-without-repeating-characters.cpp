d405d98e46ddfd03aa772c95c7283c78d01f8683
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-20 00:43:11 +0530

    Time: 51 ms (25.45%), Space: 19.3 MB (25.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l=0; int r=0; int maxlen=0;
-        unordered_map<char,int>mpp; 
-        while(r<s.size()){
+        int l=0; int r=0; int n=s.size(); 
+        unordered_map<char, int>mpp; 
+        int ans=0; 
+
+        while(r<n){
             mpp[s[r]]++; 
-            while(mpp[s[r]]>1){
-                mpp[s[l]]--; 
-                l++; 
+            if(mpp[s[r]]>1){
+                while(mpp[s[r]]>1){
+                    mpp[s[l]]--; 
+                    l++; 
+                }
             }
-            maxlen= max(maxlen, r-l+1); 
+            ans= max(ans, r-l+1); 
             r++; 
         }
-    return maxlen; 
+        
+        return ans; 
     }
 };
