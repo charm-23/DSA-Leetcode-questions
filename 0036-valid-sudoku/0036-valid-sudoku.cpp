c159fdc5fea1b63c9801ef8a2d09b783a0b28cd0
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-24 01:29:42 +0530

    Time: 0 ms (100%), Space: 23.7 MB (46.81%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,39 +4,36 @@ public:
         for(int i=0; i<9; i++){
             set<char>s; 
             for(int j=0; j<9; j++){
-                if(board[i][j]=='.') continue; 
-                if(!isdigit(board[i][j])) return false; 
-                if(s.count(board[i][j])) return false; 
-                else s.insert(board[i][j]); 
+                if(board[i][j]!='.' && s.find(board[i][j])!=s.end()){
+                    return false; 
+                }
+                s.insert(board[i][j]);
             }
         }
 
         for(int j=0; j<9; j++){
             set<char>s; 
             for(int i=0; i<9; i++){
-                if(board[i][j]=='.') continue; 
-                if(!isdigit(board[i][j])) return false; 
-                if(s.count(board[i][j])) return false; 
-                else s.insert(board[i][j]); 
+                if(board[i][j]!='.' && s.find(board[i][j])!=s.end()){
+                    return false; 
+                }
+                s.insert(board[i][j]);
             }
         }
 
         for(int row=0; row<9; row=row+3){
             for(int col=0; col<9; col=col+3){
-
                 set<char>s; 
-
                 for(int i=row; i<row+3; i++){
                     for(int j=col; j<col+3; j++){
-                        if(board[i][j]=='.') continue; 
-                        if(!isdigit(board[i][j])) return false; 
-                        if(s.count(board[i][j])) return false; 
-                        else s.insert(board[i][j]);
+                        if(board[i][j]!='.' && s.find(board[i][j])!=s.end()){
+                        return false; 
+                        }
+                        s.insert(board[i][j]);
                     }
                 }
             }
         }
-
         return true; 
     }
 };
