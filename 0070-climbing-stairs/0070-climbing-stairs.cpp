9d48fae978659198180ec759410076c1191669e4
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-24 00:41:46 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.95%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1 || n==0) return 1; 
-        int prev1=1; 
-        int prev2=1; 
-        int curr=0; 
+        vector<int>dp(n+1); 
+        dp[0]=1; dp[1]=1; 
+
         for(int i=2; i<=n; i++){
-            curr= prev1+prev2;
-            prev2=prev1; 
-            prev1=curr; 
+            dp[i]=dp[i-1]+ dp[i-2]; 
         }
-        return curr; 
+
+        return dp[n]; 
     }
 };
