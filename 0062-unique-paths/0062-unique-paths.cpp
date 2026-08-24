e99323fd1a1f5e7d5974bdb0a1f7bede828c3521
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-08-24 15:24:16 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n){
-        vector<int>row(n,1);
-        for(int i=1; i<m; i++){
-            vector<int>newrow(n,1); 
-            for(int j=1; j<n; j++){
-                newrow[j]= newrow[j-1]+ row[j]; 
-            }
-            row=newrow; 
-        }
-        return row[n-1]; 
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n,-1)); 
+        return helper(0,0,m,n,dp); 
     }
 
-    // int helper(int i, int j, int m, int n, vector<vector<int>>&dp){
-    //     if(i==m-1 && j==n-1) return 1; 
-    //     if(i==m || j==n) return 0; 
+    int helper(int i, int j, int m, int n, vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1) return 1; 
 
-    //     if(dp[i][j]!=-1) return dp[i][j]; 
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        int right=0; int down=0; 
 
-    //     int down=0; int right=0; 
-    //     if(i+1<m){
-    //         down= helper(i+1, j, m, n,dp); 
-    //     }
+        if(i+1<m){
+            down=helper(i+1, j, m, n,dp); 
+        }
 
-    //     if(j+1<n){
-    //         right= helper(i, j+1, m, n,dp); 
-    //     }
-    //     return dp[i][j]=down+right; 
-    // }
+        if(j+1<n) right= helper(i, j+1, m, n,dp); 
+
+        return dp[i][j]=down+right; 
+    }
 };
