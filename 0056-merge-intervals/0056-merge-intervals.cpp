9f5b557f5266430e850a92ee6c45006d224f475f
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-09-15 14:56:45 +0530

    Time: 4 ms (66.63%), Space: 23.8 MB (60.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,22 +5,19 @@ public:
         vector<vector<int>>ans; 
         if(intervals.size()==1) return intervals;
 
-        int i=1; 
-        int n=intervals.size(); 
-        int start=intervals[0][0]; 
-        int end= intervals[0][1]; 
+        int i=0; 
+        int n=intervals.size();
+        int start; int end; 
 
         while(i<n){
+            start=intervals[i][0];
+            end=intervals[i][1];
+
             while(i<n && intervals[i][0]<=end){
                 end=max(end, intervals[i][1]); 
                 i++; 
             }
             ans.push_back({start,end}); 
-
-            if(i<n && intervals[i][0]>end){
-                start=intervals[i][0]; 
-                end=intervals[i][1]; 
-            }
         }
 
         return ans; 
