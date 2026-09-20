5298c8ead58d021477b37f71e71c9669458739eb
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-09-20 12:44:54 +0530

    Time: 2 ms (19.75%), Space: 9.8 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        vector<int>arr(26, 0); 
         int ans=0; 
 
-        for(char c: s){
-            arr[(c-'a')]= 26-(c-'a'); 
-        }
-
         for(int i=0; i<s.size(); i++){
-            ans+= (i+1)*(arr[s[i]-'a']);
+            
+            int val= 26-(s[i]-'a'); 
+
+            ans+= (i+1)*(val);
         }
 
         return ans; 
