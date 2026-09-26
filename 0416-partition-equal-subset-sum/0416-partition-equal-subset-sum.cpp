05ec7ba5d77b83188975ee380e6a725a51f5fdf8
charm-23 <161016003+charm-23@users.noreply.github.com>
2026-09-26 12:19:25 +0530

    Time: 151 ms (37.95%), Space: 21 MB (54.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n=nums.size(); 
-        int total= accumulate(nums.begin(), nums.end(), 0); 
-        if(total%2!=0) return false; 
+        int sum= accumulate(nums.begin(), nums.end(), 0); 
 
-        vector<vector<int>>dp(n, vector<int>((total/2)+1,-1)); 
-        return helper(0, total/2, nums, dp); 
-    }
-    int helper(int i, int target, vector<int>& nums, vector<vector<int>>&dp){
-        if(target==0) return true; 
+        if(sum%2!=0) return false; 
 
-        if(i==nums.size()) return false; 
+        int target= sum/2; 
 
-        if(dp[i][target]!=-1) return dp[i][target]; 
+        vector<vector<bool>>dp(n, vector<bool>(sum+1, 0)); 
 
-        bool nottake= helper(i+1, target, nums, dp); 
-        bool take=false; 
-
-        if(nums[i]<=target){
-            take= helper(i+1, target-nums[i], nums, dp); 
+        for(int i=0; i<n; i++){
+            dp[i][0]=true; 
         }
 
-        return dp[i][target]= take || nottake; 
+        if(nums[0]<=target) dp[0][nums[0]]=true; 
+
+        for(int i=1; i<n; i++){
+            for(int sum=1; sum<=target; sum++){
+                bool nottake= dp[i-1][sum]; 
+
+                bool take=false; 
+                if(nums[i]<=sum) take=dp[i-1][sum-nums[i]]; 
+
+                dp[i][sum]= take || nottake; 
+            }
+        }
+
+        return dp[n-1][target]; 
     }
 };
